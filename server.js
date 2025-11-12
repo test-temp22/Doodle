d287d52c0da1305c3ab02a46600c0a1c73ea94a5
shaurya <sharmashaurya2907@gmail.com>
2025-11-13 00:03:52 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("draw", (data) => socket.broadcast.emit("draw", data));
+  socket.on("clear", () => socket.broadcast.emit("clear"));
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
